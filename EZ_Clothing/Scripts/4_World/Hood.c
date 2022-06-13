1c34e8689f524afc384d841018c4f355159a3d9d
Progger <progger@t-online.de>
2022-06-14 01:13:40 +0200

    fg
    dfgdfg

--- EZ_Clothing/Scripts/4_World/Hood.c
+++
@@ -1,4 +0,0 @@
-class EZ_bandit_hood_base extends Clothing {};
-class EZ_bandit_hood_olive extends EZ_bandit_hood_base {};
-class EZ_bandit_hood_black extends EZ_bandit_hood_base {};
-class EZ_bandit_hood_leaf extends EZ_bandit_hood_base {};

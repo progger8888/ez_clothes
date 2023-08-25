b82141e7d58d8c158c637b000a57a4484d5cd120
Kokaskale <kokaskale@hotmail.com>
2023-08-25 20:58:45 +0300

    fix

--- EZ_Clothing/data/factions/config.cpp
+++ EZ_Clothing/data/factions/config.cpp
@@ -1,33 +1,11 @@
 class CfgPatches
 {
-	class EZ_Clothing
+	class EZ_Factions
 	{
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
 		requiredAddons[] = {"DZ_Characters","DZ_Characters_Backpacks","DZ_Characters_Gloves","DZ_Characters_Headgear","DZ_Characters_Pants","DZ_Characters_Masks","DZ_Characters_Shoes","DZ_Characters_Tops","DZ_Characters_Vests","DZ_Data","DZ_Gear_Containers","EZ_Medal","EZ_BannerStand","ModularVestSystem","Mass_Textures"};
-	};
-};
-class CfgMods
-{
-	class EZ_Clothing
-	{
-		dir = "EZ_Clothing";
-		hideName = 1;
-		hidePicture = 1;
-		name = "EZ_Clothing";
-		version = "1.0";
-		extra = 0;
-		type = "mod";
-		dependencies[] = {"World"};
-		class defs
-		{
-			class worldScriptModule
-			{
-				value = "";
-				files[] = {"EZ_Clothing/Scripts/4_World"};
-			};
-		};
 	};
 };
 class CfgVehicles

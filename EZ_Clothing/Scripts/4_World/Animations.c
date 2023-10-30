d193d9ccfe02aa1b5ad902c15a70ea9cc9598b14
Progger <progger@t-online.de>
2023-10-30 01:00:52 +0100

    anims
    anims

--- EZ_Clothing/Scripts/4_World/Animations.c
+++ EZ_Clothing/Scripts/4_World/Animations.c
@@ -5,5 +5,7 @@ modded class ModItemRegisterCallbacks
     	super.RegisterOneHanded(pType, pBehavior);
 		
 		pType.AddItemInHandsProfileIK("EZ_CamoPaint_Base", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/spraycan.anm");
+		pType.AddItemInHandsProfileIK("EZ_Cassetteradio", "dz/anims/workspaces/player/player_main/props/player_main_1h_radio.asi", pBehavior, "dz/anims/anm/player/ik/gear/Radio.anm");
+		pType.AddItemInHandsProfileIK("EZ_Cassette_ColorBase", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/cassette.anm");
 	}
 };
